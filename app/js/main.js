13e474f076c70af749676abecdc0c2f783afc5d4
r1f-dev <r1f@mail.ru>
2021-04-01 20:49:18 +0300

    1 lessot Glee done

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,12 @@ $(document).ready(function() {
   $('.top-slider__inner').slick({
     arrows: false,
     dots: true
-  })
+  });
+
+  $('.navigation__btn').on('click', function() {
+    $('.navigation__list').toggleClass('navigation__list--active');
+  });
+  
+  var mixer = mixitup('.products__items');
 
 })
